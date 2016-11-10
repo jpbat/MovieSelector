adef4bd59d5ee48479d1e5ec19ea8f7e07d58a19
Joao Ferreira <jooaooferreira@gmail.com>
2016-11-10 18:56:04 +0100

    Changed db to MySQL

--- MovieSelector/settings.py
+++ MovieSelector/settings.py
@@ -86,8 +86,13 @@ WSGI_APPLICATION = 'MovieSelector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'movie_selector',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'CONN_MAX_AGE': 3600,
     }
 }
 
